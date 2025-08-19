a192b3389b3ed4800c33c6fc508d4cc43228ec79
Avinash Mundolli <avinashmundolli@gmail.com>
2025-08-19 10:17:29 +0530

    java

---
+++ src/test/java/com/xerofy/journalApp/JournalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xerofy.journalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
